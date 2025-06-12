219c4ccc4f11ccd59d6b519316206b1268c7e8d6
Tazeen Zahra <tazeenzahra13@gmail.com>
2025-06-12 20:30:31 +0200

    addded new text

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const PORT = 5000;
 app.use(cors());
 
 app.get('/api', (req, res) => {
-  res.json({ message: "Hello from Node.js backend!" });
+  res.json({ message: "Hello from Node.js backend hi!" });
 });
 
 app.listen(PORT, () => {
