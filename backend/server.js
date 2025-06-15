483dda623fe313b7dfaf44de2c2857b129a67a4c
Tazeen Zahra <tazeenzahra13@gmail.com>
2025-06-15 14:11:12 +0200

    new ip adress added

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 10000;
 app.use(cors());
 
 app.get('/api', (req, res) => {
-  res.json({ message: "hello from backend with updated text " });
+  res.json({ message: "hello from backend with updated text and new ip adress" });
 });
 
 app.listen(PORT, '0.0.0.0', () => {
